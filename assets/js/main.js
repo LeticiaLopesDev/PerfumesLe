748e1249f0c8b5191085d58d911a035d046a6ba9
Letícia Lopes <leticiaflopes@outlook.com>
2023-09-16 11:59:34 -0300

    primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//$img.css("background-image", "url('" + url.replace(/(\r\n|\n|\r)/gm, "") + "')");
+
+/*
+    var url = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg";
+    url = url.replace(/(\r\n|\n|\r)/gm, "");
+    var div = document.getElementById("container");
+    div.style.backgroundImage = `url(${url})`;
+    div.style.width = "640px";
+    div.style.height = "374px";
+*/
